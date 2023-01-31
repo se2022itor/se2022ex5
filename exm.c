fb2a6865aeb480e7ef27dd84e731dfd5e6d3d359
202103258 <r202103258xk@jindai.jp>
2023-01-31 22:55:00 +0900

    a

---
+++ exm.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("1~5\n");
+    for(i=1; i<=5; i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
---
+++ prc.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int x;
+int y,z;
+x=2;
+printf("x?=%d", x);
+return 0;
+}
