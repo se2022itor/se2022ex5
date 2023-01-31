9608f0d01c74307dab2e05e3a953c06ad5f3deae
202103258 <r202103258xk@jindai.jp>
2023-01-31 23:08:50 +0900

    aa

--- vscode1/code.c
+++ vscode1/code.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
 int main(void){
-    int i=10;
-    for(i=0; i<10; i++)
+    int i=0;
+    while (i<10)
+    {
+    
     printf("hello %d", i);
+    i++;
+    }
     return 0;
 }
